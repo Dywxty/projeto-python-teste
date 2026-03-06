588d733b03ddfbad0c96ba251988d4e3e4dfe01f
Dywxty <leticia.talachia@gmail.com>
2026-03-06 10:04:50 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return "<h1>Hello, World!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>Sobre a página e sua criadora:</h1><p>Esta é uma página simples criada com Flask para um teste em uma aula de python, no curso de Desenvolvimento de Sistemas</p>"
 
 if __name__ == "__main__":
     app.run()
